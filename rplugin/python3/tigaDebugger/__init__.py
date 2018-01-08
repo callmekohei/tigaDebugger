4b8cc29ea2667b017036c532057eea2850460628
callmekohei <callmekohei@gmail.com>
2018-01-08 12:23:40 +0900

    fix : direct param

--- rplugin/python3/tigaDebugger/__init__.py
+++ rplugin/python3/tigaDebugger/__init__.py
@@ -70,8 +70,8 @@ class TigaDebugger(object):
             self.util.print_cmd('Sorry! Now not avairable!')
 
 
-    def set_debugerImpl(self,args):
-        self.td   = td(self.vim)
+    def set_debugerImpl(self,args,td):
+        self.td = td(self.vim)
         self.tiga_already_set_debugger = True
         self.util.print_cmd('set {s}'.format(s=args[0]))
 
@@ -80,8 +80,7 @@ class TigaDebugger(object):
     def tiga(self,args):
 
         if self.tiga_already_set_debugger:
-            cmd_str = self.td.tiga(args)
-            self.nr = self.vim.eval(cmd_str)
+            self.nr = self.vim.eval(self.td.tiga(args))
             self.td.set_nr(self.nr)
             self.vim.command("wincmd p")
             self.tiga_debug_mode = True
@@ -104,18 +103,15 @@ class TigaDebugger(object):
 
     @neovim.function("Tiga_Run", sync=False)
     def tiga_Run(self,args):
-        cmd_str = self.td.tiga_Run()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Run())
 
     @ neovim.function("Tiga_Kill", sync=False)
     def tiga_Kill(self,arg):
-        cmd_str = self.td.tiga_Kill()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Kill())
 
     @ neovim.function("Tiga_Reset", sync=False)
     def tiga_Reset(self,arg):
-        cmd_str = self.td.tiga_Reset()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Reset())
         self.vim.command(":highlight clear CursorLine")
         self.vim.command(':sign unplace *')
 
@@ -124,36 +120,30 @@ class TigaDebugger(object):
 
     @neovim.function("Tiga_StepOver", sync=False)
     def tiga_StepOver(self,args):
-        cmd_str = self.td.tiga_StepOver()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_StepOver())
 
     @neovim.function("Tiga_StepInto", sync=False)
     def tiga_StepInto(self,args):
-        cmd_str = self.td.tiga_StepInto()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_StepInto())
 
     @ neovim.function("Tiga_StepOut", sync=True)
     def tiga_StepOut(self,arg):
-        cmd_str = self.td.tiga_StepOut()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_StepOut())
 
     @neovim.function("Tiga_Continue", sync=False)
     def tiga_Continue(self,args):
-        cmd_str = self.td.tiga_Continue()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Continue())
 
 
     ### add and clear breakpoints
 
     @neovim.function("Tiga_Breakpoints", sync=False)
     def tiga_Breakpoints(self,args):
-        cmd_str = self.td.tiga_Breakpoints()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Breakpoints())
 
     @ neovim.function("Tiga_BreakpointsAllClear", sync=False)
     def tiga_BreakpointsAllClear(self,arg):
-        cmd_str = self.td.tiga_BreakpointsAllClear()
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_BreakpointsAllClear())
         self.vim.command(':sign unplace *')
 
 
@@ -161,18 +151,15 @@ class TigaDebugger(object):
 
     @ neovim.function("Tiga_Print", sync=False)
     def tiga_Print(self,args):
-        cmd_str = self.td.tiga_Print(args)
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_Print(args))
 
     @ neovim.function("Tiga_WatchAdd", sync=False)
     def tiga_WatchAdd(self,args):
-        cmd_str = self.td.tiga_WatchAdd(args)
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_WatchAdd(args))
 
     @ neovim.function("Tiga_WatchDel", sync=False)
     def tiga_WatchDel(self,args):
-        cmd_str = self.td.tiga_WatchDel(args)
-        self.tiga_Command(cmd_str)
+        self.tiga_Command(self.td.tiga_WatchDel(args))
 
 
 ### ------------------------------------------------------------------------
